24afb5aa2db9d41ab9d21eabc43f498ac014b073
paladox <paladox@users.noreply.github.com>
2018-01-15 22:45:00 +0000

    Update ufw.pp

--- modules/base/manifests/ufw.pp
+++ modules/base/manifests/ufw.pp
@@ -2,6 +2,16 @@
 class base::ufw {
     include ::ufw
 
+    ufw::allow { 'http':
+        proto => 'tcp',
+        port  => 80,
+    }
+
+    ufw::allow { 'https':
+        proto => 'tcp',
+        port  => 443,
+    }
+
     ufw::allow { 'ssh':
         proto => 'tcp',
         port  => 22,
